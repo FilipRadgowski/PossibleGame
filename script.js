d91e2fc1c62763b022ffa532b4b7ffa5279457d6
FilipRadgowski <filip.radgowski@tm1.edu.pl>
2024-12-06 17:03:13 +0100

    Significant changes, mainly: - Smooth movement - Better bounce physics - Code optimalisation
    Co-authored-by: hamst3rpisk <hamst3rpisk@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ startButton.addEventListener("click",function(e){
     setTimeout(function() {startButton.classList.remove("pulsingClick")},2000);
     setInterval(moveEnemy, 10);
     setInterval(checkCollision, 10);
-    setInterval(checkWin, 10);
+    setInterval(checkWin, 1);
     setInterval(playerMovement, 1);
 
     startButton.classList.add("hidden");
@@ -16,17 +16,19 @@ const enemiesObj = {
 
 const player = document.querySelector("#player");
 const enemies = document.querySelectorAll(".enemy");
-console.log(enemies);
-const goal = document.querySelector("#goal1");
-
+const goals = document.querySelectorAll(".goal");
+const walls = document.querySelectorAll(".wall");
+const stuckText = document.querySelector("#stuckText");
 //-1 - up
 //1 - down
-
+player.wallFlag = false;
 for (const enemy of enemies) {
     enemy.movementFlag = -1;
     enemy.speedFlag = 1;
     if (enemy.getAttribute("id") == "enemy1") enemy.speedFlag = 2;
     if (enemy.getAttribute("id") == "enemy2") enemy.speedFlag = 3;
+    if (enemy.getAttribute("id") == "enemy3") enemy.speedFlag = 5;
+    
 }
 
 function moveEnemy () {
@@ -53,16 +55,47 @@ function checkCollision() {
             player.style.setProperty("left", 10 +"px");
         }
     }   
+    for (const wall of walls) {
+        wlcord = wall.getBoundingClientRect();
+        plcord = player.getBoundingClientRect();
+        let overlap = !(wlcord.right < plcord.left || wlcord.left > plcord.right || wlcord.bottom < plcord.top || wlcord.top > plcord.bottom);
+        if (overlap){
+            player.wallFlag = true;
+            stuckText.innerHTML = "Stuck!";
+        }
+        else {
+            player.wallFlag = false;
+            stuckText.innerHTML = "Ur safe";
+        }
+    }
 }
 
+//let goalsAchieved = 0;
+//console.log(goals.length);
 function checkWin() {
-    glcord = goal.getBoundingClientRect();
-    plcord = player.getBoundingClientRect();
-    let overlap = !(glcord.right < plcord.left || glcord.left > plcord.right || glcord.bottom < plcord.top || glcord.top > plcord.bottom);
-    if (overlap){
-        player.style.setProperty("top", window.innerHeight/2 + "px");
-        player.style.setProperty("left", 10 +"px");
+    for (const goal of goals)
+    {
+        glcord = goal.getBoundingClientRect();
+        plcord = player.getBoundingClientRect();
+        let overlap = !(glcord.right < plcord.left || glcord.left > plcord.right || glcord.bottom < plcord.top || glcord.top > plcord.bottom);
+        if (overlap){
+            
+            //goalsAchieved++;
+            //if (goalsAchieved == goals.length) {
+            player.style.setProperty("top", window.innerHeight/2 + "px");
+            player.style.setProperty("left", 10 +"px");
+            //}
+            goal.classList.add("win");
+            setTimeout(() => {
+                goal.style.setProperty("visibility","hidden");
+                goal.classList.remove("win");
+            },1000 );
+            
+
+        }
+        
     }
+    
 }
 //Player movement keyboard trigger//
 // (what rhymes with trigger? (those who know 💀))
@@ -87,10 +120,10 @@ function playerMovement() {
     if (keys[83] && !keys[87]) {
         player.style.setProperty("top",player.offsetTop + 1 +"px");
     }
-    if (keys[68] && !keys[65]) {
+    if (keys[68] && !keys[65] && !player.wallFlag) {
         player.style.setProperty("left",player.offsetLeft + 1 +"px");
     }
-    if (keys[65] && !keys[68]) {
+    if (keys[65] && !keys[68] && !player.wallFlag) {
         player.style.setProperty("left",player.offsetLeft - 1 +"px");
     }
 }
